59d0f22608d4d5e86be8f51e9c83901975254da0
Kiran-200596 <kiransgowda2005@gmail.com>
2025-06-29 19:33:51 +0530

    Add files via upload

---
+++ bio-data sample template.py
@@ -0,0 +1,32 @@
+name                      = "B S KIRAN KUMAR."
+city                      = "Channapatna."
+age                       =  29
+gender                    = "Male."
+educational_qualification = "BE in Civil Engineering."
+work_experience           = "Customer Relations and Operations Manager."
+skills                    = "Python,ML-OPs,Seaborn ,Bokeh,Matplotlib,Numpy,Pandas."
+languages_known           = "English,Kannada,Hindi."
+hobbies                   = "Watching movies,Indoor games,Listening songs,Cooking."
+marital_status            = "Single."
+
+print("      ***** This is my Bio-Data *****   ")
+print("                                        ")
+print("Name                       :"  ,name)
+print("                                        ")
+print("City                       :"   ,city)
+print("                                        ")
+print("Age                        :"   ,age)
+print("                                        ")
+print("Gender                     :"   ,gender)
+print("                                        ")
+print("Educational Qualification  :"   ,educational_qualification)
+print("                                        ")
+print("Work Experience            :"   ,work_experience)
+print("                                        ")
+print("Skills Acquired            :"   ,skills)
+print("                                        ")
+print("Languages Known            :"   ,languages_known)
+print("                                        ")
+print("Hobbies                    :"   ,hobbies)
+print("                                        ")
+print("Marital Status             :"   ,marital_status)
